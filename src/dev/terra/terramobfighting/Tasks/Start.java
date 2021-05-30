2c29d73268ff04be0a54a3e494991def63da0ad9
MICAIAHSPC\cake <micjon89@gmail.com>
2021-05-30 00:22:13 -0400

    update

--- src/dev/terra/terramobfighting/Tasks/Start.java
+++ src/dev/terra/terramobfighting/Tasks/Start.java
@@ -3,7 +3,9 @@ package dev.terra.terramobfighting.Tasks;
 import dev.terra.terramobfighting.Main;
 import dev.terra.terramobfighting.events.PlayerJoin;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -25,6 +27,23 @@ public class Start extends BukkitRunnable {
             player.teleport(location);
 
         }
+
+        Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "Spawning Zombies!");
+        Location location1 = new Location(Bukkit.getWorld("world"), -5, 100, 3);
+        location1.getWorld().spawnEntity(location1, EntityType.ZOMBIE);
+
+        Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "Spawning Zombies!");
+        Location location2 = new Location(Bukkit.getWorld("world"), 5, 100, 4);
+        location2.getWorld().spawnEntity(location2, EntityType.ZOMBIE);
+
+        Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "Spawning Zombies!");
+        Location location3 = new Location(Bukkit.getWorld("world"), -9, 100, -8);
+        location3.getWorld().spawnEntity(location3, EntityType.ZOMBIE);
+
+        Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "Spawning Zombies!");
+        Location location4 = new Location(Bukkit.getWorld("world"), 3, 100, -5);
+        location4.getWorld().spawnEntity(location4, EntityType.ZOMBIE);
+
     }
 
 }
